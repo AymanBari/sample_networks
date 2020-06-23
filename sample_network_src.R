03f64d71564e54d693ee7dd3fdb69f4b5c0c3912
Ayman Bari <31167056+AymanBari@users.noreply.github.com>
2020-06-23 06:38:07 +0300

    Update get_random_names func.
    Ensure no duplicates, keep all columns

--- sample_network_src.R
+++ sample_network_src.R
@@ -12,7 +12,7 @@ library(babynames)  # For a list of unique names to add to a social graph
 ##### FUNCTIONS #####
 # Get random list of names for graph/network nodes 
 get_random_names <- function(n) { 
-  unique_babynames <- distinct(babynames, name)                   # Filter out duplicate names 
+  unique_babynames <- distinct(babynames, name, .keep_all = TRUE) # Filter out duplicate names 
   index <- sample(1:nrow(unique_babynames), n, replace = FALSE)   # Sample of n from unique_babynames 
   names <- unique_babynames[index, ]                              # Select rows sample index 
   names                                                           # Return df of n names + attrs 
